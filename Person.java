eceb8cefda976d44b219a275947bd4328fb1fa16
yzellington <wangc19@miamioh.edu>
2017-11-01 17:55:13 -0400

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,18 +1,20 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Person {
 
     private String name;
     private String username;
     private String password;
-    private ArrayList<String> favoriteCategories = new ArrayList<String>();
+    private ArrayList<Category> favoriteCategories = new ArrayList<Category>();
     private ArrayList<Application> favoriteApplications = new ArrayList<Application>();
 
     public Person(String pName, String pUsername, String pPassword) {
         name = pName;
         username = pUsername;
         password = pPassword;
-
+        favoriteCategories = new ArrayList<Category>();
+        favoriteApplications = new ArrayList<Application>();
     }
 
     public String getName() {
@@ -27,8 +29,9 @@ public class Person {
         return username;
     }
 
-    public void makeComment(String comment) {
-        //TODO
+    public Comment makeComment(String comment) {
+       Comment a = new Comment(comment);
+       return a;
     }
 
     public void voteCommentUp(Comment comment) {
@@ -39,16 +42,20 @@ public class Person {
         comment.decrementVotes();
     }
 
-    public void voteApp() {
-        //TODO
+    public void voteApp(Application app) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("type your feeling about this application, write good or bad");
+        String feeling = in.nextLine();
+        if(feeling.equals("good")){app.addVote();}
+        else{app.deleteVote();}
     }
 
     public void addToFavorites(Application app) {
-        //TODO
+    	favoriteApplications.add(app);
     }
 
     public ArrayList<Category> getFavorites(){
-        //TODO
+        return favoriteCategories;
     }
 
     public void submitApp(Person person, Application app) {
