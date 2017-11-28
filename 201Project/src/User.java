bcda166aab6fa7f5c38e0def73439e6dddbc2350
Litong Zhang <zhangl36@miamioh.edu>
2017-11-28 16:12:05 -0500

    Add files via upload

--- 201Project/src/User.java
+++ 201Project/src/User.java
@@ -5,10 +5,20 @@ public class User extends Person{
 		super(name,username,password);
 	}
 	public void vote(Person p){
-		
+		p.beVoted();
 	}
-	public void applyModerator(){
-		
+	public Moderator applyModerator(){
+		int number = this.getVotedNumber();
+		if(number>300) {
+			Moderator a = new Moderator(this.getName(),this.getUsername(),this.getPassword());
+			a.setVotedNumber(this.getVotedNumber());
+			return a;
+		}
+		else {
+			System.out.print("You can not be the moderator now.");
+			return null;
+		}
 	}
+	
 	
 }
