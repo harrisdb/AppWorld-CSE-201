7ba8295367045f57e0a9974eeeaf548f17af913d
thatonekiddevin <ojustmute@gmail.com>
2017-12-06 17:33:33 -0500

    bug fixing/updates

---
+++ grails/AppWorld/src/main/java/myPackage/Admin.java
@@ -0,0 +1,25 @@
+package myPackage;
+public class Admin extends Moderator{
+
+	public Admin(String pName, String pUsername, String pPassword) {
+		super(pName, pUsername, pPassword);
+		this.status ="Admin";
+	}
+	
+	
+	public User revoke() {
+		return super.revoke();
+	}
+	
+	public User removeOtherModerator(Moderator a) {
+		User result = new User(a.getName(),a.getUsername(),a.getPassword());
+		result.setVotedNumber(a.getVotedNumber());
+		return result;
+	}
+	
+	public Moderator setAModerator(User a) {
+		Moderator result = new Moderator(a.getName(),a.getUsername(),a.getPassword());
+		result.setVotedNumber(a.getVotedNumber());
+		return result;
+	}
+}
